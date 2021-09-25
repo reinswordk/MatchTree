52506f5dc65c7cde8b3b8268f72889a3958c8bfc
Muhamad Hariyanto <reinswordk@gmail.com>
2021-09-25 22:44:44 +0700

    First commit

---
+++ Assets/Scripts/UIGameover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIGameover : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
